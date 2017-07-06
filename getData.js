be9c5f16ec6e7fd7aa71cdc1e97e1589e8e05416
James Lane <lanejames35@gmail.com>
2017-07-06 12:32:36 -0400

    add durham shapes

--- getData.js
+++ getData.js
@@ -2,9 +2,9 @@ function httpGetAsync(){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-            var mapData = xmlHttp.responseText;
+            return var mapData = xmlHttp.responseText;
 		}
     }
     xmlHttp.open("GET", "https://opendata.arcgis.com/datasets/7267d01b1348498694d887b091d735c7_12.geojson", true);
-    xmlHttp.send(null);
+    xmlHttp.send();
 }
